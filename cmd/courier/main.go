b1b0fe6db74fe61190d0bef2dffd378cc11c549a
gvma <gvma@ic.ufal.br>
2019-10-17 11:44:38 -0300

    Adicionando o pushinho

--- cmd/courier/main.go
+++ cmd/courier/main.go
@@ -44,6 +44,7 @@ import (
 	_ "github.com/nyaruka/courier/handlers/novo"
 	_ "github.com/nyaruka/courier/handlers/playmobile"
 	_ "github.com/nyaruka/courier/handlers/plivo"
+	_ "github.com/nyaruka/courier/handlers/pushinho"
 	_ "github.com/nyaruka/courier/handlers/redrabbit"
 	_ "github.com/nyaruka/courier/handlers/shaqodoon"
 	_ "github.com/nyaruka/courier/handlers/smscentral"
