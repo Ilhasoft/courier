973a105c1b9b17f7084b6dcb1a34316126181b10
Robi9 <robertasousa714@gmail.com>
2023-02-09 17:20:40 -0300

    Fix link

--- handlers/facebookapp/facebookapp.go
+++ handlers/facebookapp/facebookapp.go
@@ -1412,12 +1412,11 @@ func (h *handler) sendCloudAPIWhatsappMsg(ctx context.Context, msg courier.Msg) 
 			if err != nil {
 				return status, err
 			}
-
 			if attType == "application" {
 				attType = "document"
 			}
 
-			resp, err := http.Get(attURL)
+			resp, err := http.Get(parsedURL.String())
 			if err != nil {
 				return nil, err
 			}
@@ -1425,12 +1424,8 @@ func (h *handler) sendCloudAPIWhatsappMsg(ctx context.Context, msg courier.Msg) 
 
 			body := &bytes.Buffer{}
 			writer := multipart.NewWriter(body)
-
 			part, _ := writer.CreateFormFile("file", filename)
 			io.Copy(part, resp.Body)
-
-			partType, _ := writer.CreateFormField("type")
-			partType.Write([]byte(mimetype))
 
 			partMessagingProduct, _ := writer.CreateFormField("messaging_product")
 			partMessagingProduct.Write([]byte("whatsapp"))
